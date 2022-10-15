f7c9956ad42fbdd3f7e3b2c915746455b2eed456
teemo <teemo@DESKTOP-JL25QLR>
2022-10-15 16:44:17 -0300

    piano two octaves

---
+++ index.js
@@ -0,0 +1,133 @@
+
+
+var c3note = document.createElement('audio');
+var d3note= document.createElement('audio');
+var e3note = document.createElement('audio');
+var f3note = document.createElement('audio');
+var g3note = document.createElement('audio');
+var a3note = document.createElement('audio');
+var b3note = document.createElement('audio');
+var c4note = document.createElement('audio');
+var d4note = document.createElement('audio');
+var e4note = document.createElement('audio');
+var f4note = document.createElement('audio');
+var g4note = document.createElement('audio');
+var a4note = document.createElement('audio');
+var b4note = document.createElement('audio');
+var c5note = document.createElement('audio');
+
+c3note.setAttribute('src', './audios/do.mp3');
+d3note.setAttribute('src', './audios/re.mp3');
+e3note.setAttribute('src', './audios/mi.mp3');
+f3note.setAttribute('src', './audios/fa.mp3');
+g3note.setAttribute('src', './audios/sol.mp3');
+a3note.setAttribute('src', './audios/la.mp3');
+b3note.setAttribute('src', './audios/si.mp3');
+c4note.setAttribute('src', './audios/do2.mp3');
+d4note.setAttribute('src', './audios/re2.mp3');
+e4note.setAttribute('src', './audios/mi2.mp3');
+f4note.setAttribute('src', './audios/fa2.mp3');
+g4note.setAttribute('src', './audios/sol2.mp3');
+a4note.setAttribute('src', './audios/la2.mp3');
+b4note.setAttribute('src', './audios/si2.mp3');
+c5note.setAttribute('src', './audios/do3.mp3');
+
+
+
+
+$('#c').on("click",function() {
+  
+    c3note.play();
+    c3note.currentTime = 0;
+});
+
+$('#c').keypress(function() {
+ 
+    c3note.play();
+    c3note.currentTime = 0
+});
+
+
+$('#d').click(function() {
+ 
+    d3note.play();
+    d3note.currentTime = 0
+});
+
+$('#e').click(function() {
+  
+    e3note.play();
+    e3note.currentTime = 0
+});
+
+$('#f').click(function() {
+    
+    f3note.play();
+    f3note.currentTime = 0
+});
+
+$('#g').click(function() {
+    
+    g3note.play();
+    g3note.currentTime = 0
+});
+
+$('#a').click(function() {
+    
+    a3note.play();
+    a3note.currentTime = 0
+});
+
+$('#b').click(function() {
+    
+    b3note.play();
+    b3note.currentTime = 0
+});
+
+$('#c1').click(function() {
+    
+    c4note.play();
+    c4note.currentTime = 0
+});
+
+$('#d1').click(function() {
+    
+    d4note.play();
+    d4note.currentTime = 0
+});
+
+$('#e1').click(function() {
+    
+    e4note.play();
+    e4note.currentTime = 0
+});
+
+$('#f1').click(function() {
+    
+    f4note.play();
+    f4note.currentTime = 0
+});
+
+$('#g1').click(function() {
+    
+    g4note.play();
+    g4note.currentTime = 0
+});
+
+$('#a1').click(function() {
+    
+    a4note.play();
+    a4note.currentTime = 0
+});
+
+$('#b1').click(function() {
+    
+    b4note.play();
+    b4note.currentTime = 0
+});
+
+$('#c2').click(function() {
+    
+    c5note.play();
+    c5note.currentTime = 0
+});
